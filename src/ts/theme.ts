f9c5c75932a100259cb942caac98a8149578c5a4
basavarajakj2000 <basavarajakj06@gmail.com>
2023-06-21 02:13:12 +0530

    Mobile Header and Theme Implementation

--- src/ts/theme.ts
+++ src/ts/theme.ts
@@ -5,3 +5,30 @@
  */
 
 "use strict";
+
+const $HTML :HTMLElement = document.documentElement;
+const isDark :boolean = window.matchMedia("(prefers-color-scheme:dark").matches;
+
+
+if (sessionStorage.getItem('theme')) {
+    $HTML.dataset.theme = sessionStorage.getItem('theme')!;
+} else {
+    $HTML.dataset.theme = isDark ? "dark" : 'light';
+}
+
+let isPressed: boolean = false;
+
+function themeChange(this: HTMLElement) {
+    isPressed = !isPressed;
+
+    this.setAttribute("aria-pressed", String(isPressed));
+    $HTML.setAttribute("data-theme", ($HTML.dataset.theme === 'light' ? 'dark' : 'light'))
+
+    sessionStorage.setItem('theme', $HTML.dataset.theme!)
+}
+
+window.addEventListener("load", function () {
+    const $themeBtn: HTMLElement = document.querySelector("[data-theme-btn]")!;
+
+    $themeBtn.addEventListener("click", themeChange);
+} )
