288fa84ffe8ad080059e0087655328268adba313
basavarajakj2000 <basavarajakj06@gmail.com>
2023-06-25 16:47:58 +0530

    added arrow key navgations to tabs

--- src/ts/home.ts
+++ src/ts/home.ts
@@ -6,25 +6,29 @@
 
 'use strict';
 
-
 /**
  * Home page Search
  */
 
-const $SearchField: HTMLInputElement | null = document.querySelector('[data-search-field]');
-const $SearchBtn: HTMLButtonElement | null = document.querySelector('[data-search-btn]');
-const $SearchError: HTMLParagraphElement | null = document.querySelector('[data-search-error]');
+const $SearchField: HTMLInputElement | null = document.querySelector(
+	'[data-search-field]'
+);
+const $SearchBtn: HTMLButtonElement | null =
+	document.querySelector('[data-search-btn]');
+const $SearchError: HTMLParagraphElement | null = document.querySelector(
+	'[data-search-error]'
+);
 
 if ($SearchField && $SearchBtn) {
-    $SearchBtn.addEventListener('click',  function(this :HTMLElement)  {
-        if (($SearchField).value) {
-          window.location.href = `/recipes.html?q=${($SearchField as HTMLInputElement).value}`;
-        } 
-        
-        else if ($SearchError) {
-            $SearchError.setAttribute('aria-hidden', 'false');
-        }
-    });
+	$SearchBtn.addEventListener('click', function (this: HTMLElement) {
+		if ($SearchField.value) {
+			window.location.href = `/recipes.html?q=${
+				($SearchField as HTMLInputElement).value
+			}`;
+		} else if ($SearchError) {
+			$SearchError.setAttribute('aria-hidden', 'false');
+		}
+	});
 }
 
 /**
@@ -32,11 +36,11 @@ if ($SearchField && $SearchBtn) {
  */
 
 if ($SearchField && $SearchBtn) {
-    $SearchField.addEventListener("keydown", e => {
-        if (e.key === "Enter") {
-            $SearchBtn.click();
-        }
-    })
+	$SearchField.addEventListener('keydown', (e) => {
+		if (e.key === 'Enter') {
+			$SearchBtn.click();
+		}
+	});
 }
 
 /**
@@ -44,37 +48,74 @@ if ($SearchField && $SearchBtn) {
  */
 
 interface AddEventOnElementsOptions {
-    $elements: NodeList;
-    eventType: string;
-    callback: EventListenerOrEventListenerObject;
+	$elements: NodeList;
+	eventType: string;
+	callback: EventListenerOrEventListenerObject;
 }
 
- const addEventOnElements  = ({$elements, eventType, callback} :AddEventOnElementsOptions)  => {
-    for (const element of Array.from($elements)) {
-        element.addEventListener(eventType, callback);
-    }
-}
+const addEventOnElements = ({
+	$elements,
+	eventType,
+	callback,
+}: AddEventOnElementsOptions) => {
+	for (const element of Array.from($elements)) {
+		element.addEventListener(eventType, callback);
+	}
+};
 
-const $tabBtns: NodeListOf<HTMLButtonElement> = document.querySelectorAll('[data-tab-btn]');
-const $tabPanels: NodeListOf<HTMLElement> = document.querySelectorAll('[data-tab-panel]');
+const $tabBtns: NodeListOf<HTMLButtonElement> =
+	document.querySelectorAll('[data-tab-btn]');
+const $tabPanels: NodeListOf<HTMLElement> =
+	document.querySelectorAll('[data-tab-panel]');
 
 let $lastActiveTabBtn: HTMLElement | undefined = $tabBtns[0];
 let $lastActiveTabPanel: HTMLElement | undefined = $tabPanels[0];
 
+addEventOnElements({
+	$elements: $tabBtns,
+	eventType: 'click',
+	callback: function (this: HTMLElement) {
+		$lastActiveTabPanel?.setAttribute('hidden', 'true');
+		$lastActiveTabBtn?.setAttribute('aria-selected', 'false');
+		$lastActiveTabBtn?.setAttribute('tabindex', '-1');
+
+		const $currentTabPanel: HTMLElement | null = document.querySelector(
+			`#${this.getAttribute('aria-controls')}`
+		);
+
+		$currentTabPanel?.removeAttribute('hidden');
+		this.setAttribute('aria-selected', 'true');
+		this.setAttribute('tabindex', '0');
+
+		$lastActiveTabPanel = $currentTabPanel;
+		$lastActiveTabBtn = this;
+	},
+});
+
+/**
+ * Navigate Tab with arrow key
+ */
 
 addEventOnElements({
-    $elements: $tabBtns, eventType: "click", callback: function (this: HTMLElement) {
-        $lastActiveTabPanel?.setAttribute('hidden', 'true')
-        $lastActiveTabBtn?.setAttribute('aria-selected', 'false');
-        $lastActiveTabBtn?.setAttribute("tabindex", '-1');
 
-        const $currentTabPanel: HTMLElement | null = document.querySelector(`#${this.getAttribute("aria-controls")}`);
-        
-        $currentTabPanel?.removeAttribute("hidden");
-        this.setAttribute("aria-selected", 'true');
-        this.setAttribute("tabindex", '0');
-        
-        $lastActiveTabPanel = $currentTabPanel;
-        $lastActiveTabBtn = this;
-    }
+	$elements: $tabBtns,
+	eventType: 'keydown',
+	callback: function (this: HTMLElement, e :KeyboardEvent) {
+		const $nextElement: HTMLElement | null = this.nextElementSibling;
+		const $previousElement: HTMLElement | null = this.previousElementSibling;
+
+        console.log($nextElement)
+		if (e.key === 'ArrowRight' && $nextElement) {
+			this.setAttribute('tabindex', '-1');
+			$nextElement.setAttribute('tabindex', '0');
+			$nextElement.focus();
+		} else if ((e.key === 'ArrowLeft') && $previousElement) {
+			this.setAttribute('tabindex', '-1');
+			$previousElement?.setAttribute('tabindex', '0');
+			$previousElement.focus();
+        } else if (e.key === 'Tab') {
+            this.setAttribute("tabindex", '-1');
+            $lastActiveTabBtn?.setAttribute("tabindex", '0')
+		}
+	},
 });
